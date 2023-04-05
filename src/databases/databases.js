a9a39435ca4f4cfc24d536817bca1984716921b9
Sergio Augusto Moreira Bastos Junior <89741627+sergiofisio@users.noreply.github.com>
2023-04-05 14:06:31 -0300

    criado carrossel no appartments

--- src/databases/databases.js
+++ src/databases/databases.js
@@ -1,24 +1,29 @@
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+
 const apartments = [
-    {
-        name: 'apartment_five_room',
-        image: 'image_apartment_five_room',
-        info: 'information_apartment_five_room'
-    },
-    {
-        name: 'apartment_four_room',
-        image: 'image_apartment_four_room',
-        info: 'information_apartment_four_room'
-    },
-    {
-        name: 'apartment_three_room',
-        image: 'image_apartment_three_room',
-        info: 'information_apartment_three_room'
-    },
-    {
-        name: 'apartment_studio',
-        image: 'image_apartment_studio',
-        info: 'information_apartment_studio'
-    }
+  {
+    name: "apartment_five_room",
+    image: img1,
+    info: "information_apartment_five_room",
+  },
+  {
+    name: "apartment_four_room",
+    image: img2,
+    info: "information_apartment_four_room",
+  },
+  {
+    name: "apartment_three_room",
+    image: img3,
+    info: "information_apartment_three_room",
+  },
+  {
+    name: "apartment_studio",
+    image: img4,
+    info: "information_apartment_studio",
+  },
 ];
 
 export default apartments;
