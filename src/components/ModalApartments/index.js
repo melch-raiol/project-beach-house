e18f5483b6172758f6a2edecd33f734ca584bdde
melch <melchraiol@hotmail.com>
2023-05-29 13:53:55 -0300

    atualização na pagina Contato

--- src/components/ModalApartments/index.js
+++ src/components/ModalApartments/index.js
@@ -1,27 +1,26 @@
 import "./styles.css";
 
-export default function ModalApartments({ setModalApartments, selectedApartment }) {
-    console.log(selectedApartment);
-    function closeModal() {
-        setModalApartments(false);
-    };
+export default function ModalApartments({
+  setModalApartments,
+  selectedApartment,
+}) {
+  console.log(selectedApartment);
+  function closeModal() {
+    setModalApartments(false);
+  }
 
-    return (
-        <div className="container-modal" >
-            <div className="modal-info">
-                <div
-                    className="close-modal"
-                    onClick={() => closeModal()}
-                >
-                    X
-                </div>
-                <img
-                    className="modal-info-img"
-                    src={selectedApartment.image}
-                />
-                <p>{selectedApartment.name}</p>
-                <p>{selectedApartment.info}</p>
-            </div>
+  return (
+    <div className="container-modal">
+      <div className="modal-info">
+        <div className="close-modal" onClick={() => closeModal()}>
+          <span>X</span>
         </div>
-    )
-};
+        <div className="div-img-info">
+          <img className="modal-info-img" src={selectedApartment.image} />
+          <p>{selectedApartment.name}</p>
+          <p>{selectedApartment.info}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,107 +1,119 @@
-import emailjs from '@emailjs/browser';
-import { useState } from 'react';
-import ImgContact from '../../assets/contact.jpg';
-import ImgWhatsapp from '../../assets/whatsapp-icon.png';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
-import './styles.css';
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import ImgContact from "../../assets/contact.jpg";
+import ImgWhatsapp from "../../assets/whatsapp-icon.png";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import "./styles.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Contact() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-    function handleEmail(e) {
-        e.preventDefault();
+  function handleEmail(e) {
+    e.preventDefault();
 
-        if (name === '' || email === '' || subject === '' || message === '') {
-            alert("Preencha todos os campos");
-            return;
-        };
+    if (name === "" || email === "" || subject === "" || message === "") {
+      return toast.error("PREENCHA TODOS OS CAMPOS!", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
 
-        const templateParams = {
-            from_name: name,
-            message: message,
-            email: email
-        };
-
-        emailjs.send("service_luee47e", "template_6uqb2v6", templateParams, "XA3RcIqj9J19FAQkU")
-            .then((res) => {
-                console.log("Email enviado", res.status, res.text);
-                setName('');
-                setEmail('');
-                setSubject('');
-                setMessage('');
-            }, (error) => {
-                console.log("ERROR: ", error);
-            });
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
     };
 
-    return (
-        <div className="container-contact">
-            <Header />
-            <img
-                className='img-background-contact'
-                src={ImgContact}
-            />
-            <img
-                className='img-whatsapp'
-                src={ImgWhatsapp}
-            />
-            <div className='container-form'>
+    emailjs
+      .send(
+        "service_luee47e",
+        "template_6uqb2v6",
+        templateParams,
+        "XA3RcIqj9J19FAQkU"
+      )
+      .then(
+        (res) => {
+          console.log("Email enviado", res.status, res.text);
+          setName("");
+          setEmail("");
+          setSubject("");
+          setMessage("");
+        },
+        (error) => {
+          console.log("ERROR: ", error);
+        }
+      );
 
-                <div className='title-contact'>
-                    <span>Contato</span>
-                </div>
+    openAlert();
+  }
 
-                <div className='info-form'>
-                    informações
-                </div>
+  function openAlert() {
+    return toast.success("EMAIL ENVIADO COM SUCESSO", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+  }
 
-                <form
-                    onSubmit={handleEmail}
-                    className='form-contact'
-                >
-                    <input
-                        className='input-contact'
-                        type='name'
-                        placeholder='Nome'
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-                    <input
-                        className='input-contact'
-                        type='email'
-                        placeholder='Email'
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                    <input
-                        className='input-contact'
-                        type='text'
-                        placeholder='Assunto'
-                        onChange={(e) => setSubject(e.target.value)}
-                        value={subject}
-                    />
-                    <textarea
-                        className='input-contact message'
-                        type='text'
-                        placeholder='Menssagem'
-                        onChange={(e) => setMessage(e.target.value)}
-                        value={message}
-                    />
-                    <button
-                        className='input-contact button-send'
-                    >
-                        Enviar
-                    </button>
-                </form>
-            </div>
-
-            <Footer />
+  return (
+    <div className="container-contact">
+      <ToastContainer />
+      <Header />
+      <img className="img-background-contact" src={ImgContact} />
+      <img className="img-whatsapp" src={ImgWhatsapp} />
+      <div className="container-form">
+        <div className="title-contact">
+          <span>Contato</span>
         </div>
-    )
-};
+
+        <div className="div-info-and-form">
+          <div className="info-form">
+            <span className="title-info">Casas da Praia da Sepultura</span>
+            <span>Rua Chincharro, 309 - Centro - Bombinhas / SC</span>
+            <span>whatsapp: +55 47 99755-1106</span>
+            <span>casasdasepultura@gamil.com</span>
+          </div>
+
+          <form onSubmit={handleEmail} className="form-contact">
+            <input
+              className="input-contact"
+              type="name"
+              placeholder="Nome"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            <input
+              className="input-contact"
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <input
+              className="input-contact"
+              type="text"
+              placeholder="Assunto"
+              onChange={(e) => setSubject(e.target.value)}
+              value={subject}
+            />
+            <textarea
+              className="input-contact message"
+              type="text"
+              placeholder="Mensagem"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+            <button className="input-contact button-send">Enviar</button>
+          </form>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
 
 export default Contact;
