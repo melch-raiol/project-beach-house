37c7a0ccd3ffdf1d5246f0b9857cc897667a213e
melch <melchraiol@hotmail.com>
2023-06-20 13:19:53 -0300

    ultimo commit sem o Bruno

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import Whatsapp from './components/Whatsapp';
-import AboutUs from './pages/AboutUs';
-import Apartments from './pages/Apartments';
-import Book from './pages/Book';
-import Contact from './pages/Contact';
-import Main from './pages/Main';
+import { Route, Routes } from "react-router-dom";
+import Whatsapp from "./components/Whatsapp";
+import AboutUs from "./pages/AboutUs";
+import Apartments from "./pages/Apartments";
+import Book from "./pages/Book";
+import Contact from "./pages/Contact";
+import Main from "./pages/Main";
 
 function MainRoutes() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Main />} />
-                <Route path='/AboutUs' element={<AboutUs />} />
-                <Route path='/Contact' element={<Contact />} />
-                <Route path='/Book' element={<Book />} />
-                <Route path='/Apartments' element={<Apartments />} />
-            </Routes>
-            <Whatsapp />
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Book" element={<Book />} />
+        <Route path="/Apartments" element={<Apartments />} />
+      </Routes>
+    </>
+  );
 }
 
 export default MainRoutes;
